205b9b7ed8e7488955871dedd7d16c563a533bcb
Jimmie Walker <jimmiewalker653@yahoo.com>
2013-02-09 18:01:04 -0600

    testing

--- bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/mortdeus/android/workspace/malydian/bin/AndroidManifest.xml : \
-/home/mortdeus/android/workspace/malydian/AndroidManifest.xml \
--- bin/malydian-debug-unaligned.apk.d
+++
@@ -1,3 +0,0 @@
-/home/mortdeus/android/workspace/malydian/bin/malydian-debug-unaligned.apk : \
-/home/mortdeus/android/workspace/malydian/bin/malydian.ap_ \
-/home/mortdeus/android/workspace/malydian/bin/classes.dex \
